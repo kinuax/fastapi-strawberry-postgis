0147d055a3764a7d50bd5893e12ec276cf45e49a
Kinuax <kinuax@protonmail.com>
2025-05-08 10:44:30 +0200

    Add asyncpg and SQLAlchemy integration as database package

--- app/settings/__init__.py
+++ app/settings/__init__.py
@@ -1,1 +1,1 @@
-from .settings import get_settings
+from .main import get_settings
--- app/settings/main.py
+++ app/settings/main.py
@@ -7,6 +7,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=Path(__file__).parent / ".env", env_file_encoding="utf-8")
     debug: bool = False
+    postgres_drivername: str = "postgresql+asyncpg"
+    postgres_host: str = "fastrawgis-db"
+    postgres_port: int = 5432
+    postgres_database: str = "postgres"
+    postgres_username: str = "postgres"
+    postgres_password: str = "postgres"
 
 
 @lru_cache
