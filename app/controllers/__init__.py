4036f228dec44a7eca6566e063563f5127b1ea76
Kinuax <kinuax@protonmail.com>
2025-05-10 19:47:09 +0200

    Update database controllers

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,1 +1,1 @@
-from .database import create_tables, drop_tables, truncate_tables
+from .database import create_tables, load_tables, truncate_tables, drop_tables
--- app/controllers/database.py
+++ app/controllers/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import text
 
-from app.database import async_engine, Base, get_async_session
+from app.database import Base, async_engine, get_async_session
 
 
 async def create_tables() -> None:
@@ -8,17 +8,24 @@ async def create_tables() -> None:
         await connection.run_sync(Base.metadata.create_all)
 
 
-async def drop_tables(tables: list[str]) -> None:
+async def load_tables(objs: list[Base]) -> None:
     async with get_async_session() as session:
-        for table in tables:
-            drop_stmt = text(f"DROP TABLE {table}")
-            await session.execute(drop_stmt)
-        await session.commit()
+        for obj in objs:
+            session.add(obj)
+            await session.commit()
 
 
 async def truncate_tables(tables: list[str]) -> None:
     async with get_async_session() as session:
         for table in tables:
-            truncate_stmt = text(f"TRUNCATE TABLE {table}")
+            truncate_stmt = text(f"TRUNCATE TABLE {table} CASCADE")
             await session.execute(truncate_stmt)
+        await session.commit()
+
+
+async def drop_tables(tables: list[str]) -> None:
+    async with get_async_session() as session:
+        for table in tables:
+            drop_stmt = text(f"DROP TABLE {table}")
+            await session.execute(drop_stmt)
         await session.commit()
