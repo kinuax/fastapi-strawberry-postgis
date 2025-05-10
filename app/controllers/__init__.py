19ea42e88dfafccb53166a867237234f273d48e6
Kinuax <kinuax@protonmail.com>
2025-05-10 20:16:05 +0200

    Add models controllers

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,1 +1,4 @@
 from .database import create_tables, load_tables, truncate_tables, drop_tables
+from .events import get_event, get_events
+from .towns import get_town, get_towns
+from .venues import get_venue, get_venues
